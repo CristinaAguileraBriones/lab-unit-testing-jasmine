dbf740c33fa202063d62f2581c755f0577b351ec
Cristina Aguilera <developer.c.93.c@gmail.com>
2024-08-31 20:26:37 +0200

    solved

--- src/divide.js
+++ src/divide.js
@@ -1,3 +1,11 @@
-function divide () {
-       
+function divide(numOne, numTwo) {
+    if (typeof numOne !== "number" || typeof numTwo !== "number") {
+        return undefined;
+    }
+    
+    if (numTwo === 0) {
+        return Infinity; // Handle division by zero according to JavaScript behavior
+    }
+
+    return numOne / numTwo;
 }
--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,42 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
 
-describe("Iteration 2", () => {
-    describe("Function - divide", () => {
+
+describe("Iteration 1 | Getting Started", () => {
+    describe("Function divide", () => {
         
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+        it("should be defined", () => {
+            expect(divide).toBeDefined();
+        });
 
-    })    
-})
+        it("should take two arguments", () => {
+            expect(divide.length).toBe(2);
+        });
 
-*/
+        it("should return the division of the two numbers", () => {
+            expect(divide(10, 2)).toEqual(5);
+            expect(divide(20, 4)).toEqual(5);
+            expect(divide(100, 25)).toEqual(4);
+        });
+
+        it("should return undefined if any of the arguments is not provided", () => {
+            expect(divide(10)).toEqual(undefined);
+            expect(divide()).toEqual(undefined);
+            expect(divide(undefined, 2)).toEqual(undefined);
+        });
+
+        it("should return undefined if any of the arguments is not a number", () => {
+            expect(divide(10, "2")).toEqual(undefined);
+            expect(divide("20", 4)).toEqual(undefined);
+            expect(divide(true, 2)).toEqual(undefined);
+            expect(divide(100, null)).toEqual(undefined);
+        });
+
+        it("should handle division by zero correctly", () => {
+            expect(divide(10, 0)).toEqual(Infinity);
+            expect(divide(0, 10)).toEqual(0);
+        });
+
+    });
+});
+
